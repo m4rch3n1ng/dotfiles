bc1705537fbb639df8e38010bb7f955f3d6b3ef0
may <m4rch3n1ng@gmail.com>
2023-04-09 05:35:35 +0200

    added wofi config, adjusted font for waybar

---
+++ arch/.config/wofi/power-menu.sh
@@ -0,0 +1,15 @@
+# credit https://github.com/MathisP75/hyppuccin/blob/main/wofi/powermenu.sh
+
+entries="󰜉 reboot\n󰤆 shutdown\n󰗽 logout\n󰽥 suspend"
+
+selected=$(echo -e $entries | wofi --width 250 --height 260 --dmenu --hide_search --hide-scroll --cache-file /dev/null | awk '{print tolower($2)}')
+case $selected in
+	logout)
+		exec hyprctl dispatch exit NOW;;
+	suspend)
+		exec systemctl suspend;;
+	reboot)
+		exec systemctl reboot;;
+	shutdown)
+		exec systemctl poweroff -i;;
+esac
